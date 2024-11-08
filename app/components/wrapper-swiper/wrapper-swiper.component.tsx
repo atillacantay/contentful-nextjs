c8fd7b76c2f0805035f47d97b4e35955e01c7484
Atilla Gul <atillacantay@hotmail.com>
2024-11-08 09:35:22 +0300

    wrapper-swiper fix

--- app/components/wrapper-swiper/wrapper-swiper.component.tsx
+++ app/components/wrapper-swiper/wrapper-swiper.component.tsx
@@ -1,8 +1,7 @@
 "use client";
 
-import Swiper from "swiper";
 import { Navigation } from "swiper/modules";
-import { Swiper as SwiperReact } from "swiper/react";
+import { Swiper } from "swiper/react";
 
 const WrapperSwiper = ({
   children,
@@ -15,9 +14,6 @@ const WrapperSwiper = ({
   props?: [string];
   fromColor?: string;
 }) => {
-  const isDevelopment =
-    !process.env.NODE_ENV || process.env.NODE_ENV === "development";
-
   // basic rtl detect
   const isRTL = document?.documentElement?.querySelector("[dir='rtl']");
 
@@ -26,55 +22,43 @@ const WrapperSwiper = ({
     prevEl: `#${uniqueId} .navigation-sub-${isRTL ? "next" : "prev"}`,
   };
 
-  if (!isDevelopment) {
-    new Swiper(`#${uniqueId}`, {
-      slidesPerView: "auto",
-      spaceBetween: 10,
-      grabCursor: true,
-      modules: [Navigation],
-      navigation,
-    });
-  }
-
   return (
     <>
-      {isDevelopment && (
-        <SwiperReact
-          className="w-full !pb-2"
-          slidesPerView={"auto"}
-          spaceBetween={16}
-          grabCursor={true}
-          modules={[Navigation]}
-          navigation={navigation}
-          id={uniqueId}
-          style={
-            {
-              "--dark-bg-gradient-start-color": bgGradientStartColorInDarkMode,
-              "--from-color": fromColor,
-            } as React.CSSProperties
-          }
+      <Swiper
+        className="w-full !pb-2"
+        slidesPerView={"auto"}
+        spaceBetween={16}
+        grabCursor={true}
+        modules={[Navigation]}
+        navigation={navigation}
+        id={uniqueId}
+        style={
+          {
+            "--dark-bg-gradient-start-color": bgGradientStartColorInDarkMode,
+            "--from-color": fromColor,
+          } as React.CSSProperties
+        }
+      >
+        {children}
+        <div
+          className={`navigation-sub-prev absolute top-0 z-10 left-0 h-full bg-gradient-to-r from-[color:var(--from-color)] dark:from-[color:var(--dark-bg-gradient-start-color)] pr-24 [&.swiper-button-disabled]:hidden hidden sm:flex justify-center items-center`}
         >
-          {children}
-          <div
-            className={`navigation-sub-prev absolute top-0 z-10 left-0 h-full bg-gradient-to-r from-[color:var(--from-color)] dark:from-[color:var(--dark-bg-gradient-start-color)] pr-24 [&.swiper-button-disabled]:hidden hidden sm:flex justify-center items-center`}
-          >
-            <div className="relative w-10 h-10 bg-white dark:bg-goki_dark_card rounded-full border border-red-600 dark:border-primary_red_dark p-3 text-black font-[swiper-icons] p-5">
-              <span className="absolute top-0 left-0 w-full h-full text-center flex justify-center items-center cursor-pointer select-none text-red-600  dark:text-primary_red_dark">
-                prev
-              </span>
-            </div>
+          <div className="relative w-10 h-10 bg-white dark:bg-goki_dark_card rounded-full border border-red-600 dark:border-primary_red_dark p-3 text-black font-[swiper-icons] p-5">
+            <span className="absolute top-0 left-0 w-full h-full text-center flex justify-center items-center cursor-pointer select-none text-red-600  dark:text-primary_red_dark">
+              prev
+            </span>
           </div>
-          <div
-            className={`navigation-sub-next absolute top-0 z-10 right-0 h-full bg-gradient-to-l from-[color:var(--from-color)] dark:from-[color:var(--dark-bg-gradient-start-color)] pl-24 [&.swiper-button-disabled]:hidden hidden sm:flex justify-center items-center`}
-          >
-            <div className="relative w-10 h-10 bg-white dark:bg-goki_dark_card rounded-full border border-red-600 dark:border-primary_red_dark p-3 text-black font-[swiper-icons] p-5">
-              <span className="absolute top-0 left-0 w-full h-full text-center flex justify-center items-center cursor-pointer select-none text-red-600 dark:text-primary_red_dark">
-                next
-              </span>
-            </div>
+        </div>
+        <div
+          className={`navigation-sub-next absolute top-0 z-10 right-0 h-full bg-gradient-to-l from-[color:var(--from-color)] dark:from-[color:var(--dark-bg-gradient-start-color)] pl-24 [&.swiper-button-disabled]:hidden hidden sm:flex justify-center items-center`}
+        >
+          <div className="relative w-10 h-10 bg-white dark:bg-goki_dark_card rounded-full border border-red-600 dark:border-primary_red_dark p-3 text-black font-[swiper-icons] p-5">
+            <span className="absolute top-0 left-0 w-full h-full text-center flex justify-center items-center cursor-pointer select-none text-red-600 dark:text-primary_red_dark">
+              next
+            </span>
           </div>
-        </SwiperReact>
-      )}
+        </div>
+      </Swiper>
     </>
   );
 };
